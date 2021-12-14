87f7a4ed3b8c04aa796a1f74798aac3f70515015
MinJeong111 <kjmj9108@naver.com>
2021-12-14 18:36:07 +0900

    IntalliJ GitHub Test

---
+++ src/main/java/com/icia/githubtest1/GitHubTest1Application.java
@@ -0,0 +1,13 @@
+package com.icia.githubtest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubTest1Application.class, args);
+    }
+
+}
